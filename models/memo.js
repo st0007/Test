1a1bfd601183919320948559f96812040ea7ac70
isebm <isebm@jp.ibm.com>
2018-02-01 10:33:08 +0900

    first commit

---
+++ models/memo.js
@@ -0,0 +1,63 @@
+﻿// MemoApp – models\memo.js (メモリー版)
+
+// (a)メモを保持するオブジェクト
+var docs = {};
+
+// (1)メモ一覧の取得
+exports.list = function(callback) {
+  var list = Object.keys(docs).map(function(id) {
+    var row = {
+      id : id,
+      title : docs[id].title,
+      updatedAt : docs[id].updatedAt
+    };
+
+    return row;
+  }).sort(function(a, b) {
+    if (a.updatedAt < b.updatedAt)
+      return 1;
+    if (a.updatedAt > b.updatedAt)
+      return -1;
+
+    return 0;
+  });
+
+  process.nextTick(function() {
+    callback(null, list);
+  });
+};
+
+// (2)メモの取得
+exports.get = function(id, callback) {
+  var doc = {
+    title : docs[id].title,
+    content : docs[id].content,
+    updatedAt : docs[id].updatedAt
+  };
+
+  process.nextTick(function() {
+    callback(null, doc);
+  });
+};
+
+// (3)メモの保存
+exports.save = function(id, doc, callback) {
+  docs[id] = {
+    title : doc.title,
+    content : doc.content,
+    updatedAt : doc.updatedAt
+  };
+
+  process.nextTick(function() {
+    callback();
+  });
+};
+
+// (4)メモの削除
+exports.remove = function(id, callback) {
+  delete docs[id];
+
+  process.nextTick(function() {
+    callback();
+  });
+};
---
+++ reference/cloudant/memo.js
@@ -0,0 +1,42 @@
+﻿// MemoApp – models\memo.js (Cloudant版)
+
+// (a)使用モジュールの読み込み
+var cradle = require('cradle');
+
+// (b)Cloudant接続情報の取得
+var services = JSON.parse(process.env.VCAP_SERVICES);
+var credentials = services['cloudantNoSQLDB'][0].credentials;
+var host = credentials.host;
+var port = credentials.port;
+var options = {
+  cache : true,
+  raw : false,
+  secure : true,
+  auth : {
+    username : credentials.username,
+    password : credentials.password
+  }
+};
+
+// (c)メモを保持するデータベース
+var db = new (cradle.Connection)(host, port, options).database('memo');
+
+// (1)メモ一覧の取得
+exports.list = function(callback) {
+  db.view('memos/list', { descending : true }, callback);
+};
+
+// (2)メモの取得
+exports.get = function(id, callback) {
+  db.get(id, callback);
+};
+
+// (3)メモの保存
+exports.save = function(id, doc, callback) {
+  db.save(id, doc, callback);
+};
+
+// (4)メモの削除
+exports.remove = function(id, callback) {
+  db.remove(id, callback);
+};
